48ff66f83878922daf3cd8542c6bee9cb2c3f2a5
Matthew Foster <75863267+mjfoster126@users.noreply.github.com>
2023-12-26 17:27:19 -0600

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-* * * * * /usr/bin/curl --request GET 'http://golf-360-test-zwiu3.ondigitalocean.app/api/cron' >/proc/1/fd/1 2>/proc/1/fd/2
+* * * * * /usr/bin/curl http://golf-360-test-zwiu3.ondigitalocean.app/api/cron >/proc/1/fd/1 2>/proc/1/fd/2
 
 #empty
